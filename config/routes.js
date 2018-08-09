3907e0d5f6a6357069645eb28e008b526487aad9
FDMcreative <federico.delmonte@gmail.com>
2018-08-09 11:58:43 +0200

    first commit just installed

---
+++ config/routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const secureRoute = require('../lib/secureRoute');
+const sessions = require('../controllers/sessions');
+const registrations = require('../controllers/registrations');
+
+router.get('/', (req, res) => res.render('statics/index'));
+
+router.route('/register')
+  .get(registrations.new)
+  .post(registrations.create);
+
+router.route('/login')
+  .get(sessions.new)
+  .post(sessions.create);
+
+router.all('*', (req, res) => res.notFound());
+
+module.exports = router;
